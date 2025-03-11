c5ea0dfbd8eb9df43418eecbc038fae91f305684
Devkaran Solanki <devkaransolanki01@gmail.com>
2025-03-11 11:30:42 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,10 @@ import { PreloaderComponent } from './preloader/preloader.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { OffcanvasCartComponent } from './offcanvas-cart/offcanvas-cart.component';
-import { OffcanvasNavbarComponent } from './offcanvas-navbar/offcanvas-navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,PreloaderComponent,HeaderComponent,FooterComponent,OffcanvasCartComponent,
-    OffcanvasNavbarComponent,RouterLink],
+  imports: [RouterOutlet,PreloaderComponent,HeaderComponent,FooterComponent,OffcanvasCartComponent,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { ProducttComponent } from './productt/productt.component';
+import { ProductComponent } from './product/product.component';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
@@ -10,8 +10,8 @@ export const routes: Routes = [
     { path: 'signup', component: SignupComponent },
     { path: 'login', component: LoginComponent },
     { path: 'cart', component: CartComponent },
-    { path: 'products', component: ProducttComponent },
-    { path: 'products/:categoryId', component: ProducttComponent, runGuardsAndResolvers: 'always' },
+    { path: 'products', component: ProductComponent },
+    { path: 'products/:categoryId', component: ProductComponent, runGuardsAndResolvers: 'always' },
 ];
 
 
